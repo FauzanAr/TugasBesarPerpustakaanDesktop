ddd4299a2a7f5aa7f861e7f48f240f77b2392ee1
KhosyAkmal <akmalkhosy@gmail.com>
2018-12-11 10:44:28 +0700

    menyelesaikan class BukuJavaToDatabase

--- src/com/FearlessMans/Perpustakaan/lib/BukuJavaToDatabase.java
+++ src/com/FearlessMans/Perpustakaan/lib/BukuJavaToDatabase.java
@@ -6,6 +6,7 @@
 package com.FearlessMans.Perpustakaan.lib;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -63,5 +64,63 @@ public class BukuJavaToDatabase {
             e.printStackTrace();
         }
         return null;
+    }
+    
+    public boolean insertBuku(Buku buku) {
+        Connection koneksi = Koneksi.buka_koneksi();
+        try {
+            PreparedStatement ps = koneksi.prepareStatement("INSERT INTO buku(judul_buku, pengarang_buku, id_category_buku, jumlah_buku) VALUES (?,?,?,?)");
+            ps.setString(1, buku.getJudul());
+            ps.setString(2, buku.getPengarang());
+            ps.setInt(3, buku.getIdKategory());
+            ps.setInt(4, buku.getJumlahBuku());
+            
+            int i = ps.executeUpdate();
+            
+            if (i == 1){
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateBuku(Buku buku) {
+        Connection koneksi = Koneksi.buka_koneksi();
+        
+        try {
+            PreparedStatement ps = koneksi.prepareStatement("UPDATE buku SET judul_buku=?, pengarang_buku=?, id_category_buku=?, jumlah_buku=? WHERE id=?");
+            ps.setString(1, buku.getJudul());
+            ps.setString(2, buku.getPengarang());
+            ps.setInt(3, buku.getIdKategory());
+            ps.setInt   (4, buku.getJumlahBuku());
+            ps.setInt   (5, buku.getId());
+            
+            int i = ps.executeUpdate();
+            
+            if (i == 1){
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public boolean deleteBuku(int id) {
+        Connection koneksi = Koneksi.buka_koneksi();
+        
+        try {
+            Statement stmt = koneksi.createStatement();
+            int i = stmt.executeUpdate("DELETE FROM buku WHERE id="+id);
+            if(i == 1){
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
