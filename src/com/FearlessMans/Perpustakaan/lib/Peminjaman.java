f02057f365f45f9a0296bb6461a527fa63d2f0e9
FauzanAr <faurinta12345@gmail.com>
2018-12-19 23:12:01 +0700

    Class Peminjaman

--- src/com/FearlessMans/Perpustakaan/lib/Peminjaman.java
+++ src/com/FearlessMans/Perpustakaan/lib/Peminjaman.java
@@ -11,7 +11,51 @@ import java.util.Date;
  * @author Ojan
  */
 public class Peminjaman {
-    int idPeminjaman, idUserPerpus, idBuku;
-    Date tanggalPinjam, tanggalKembali;
+    private int idPeminjaman, idUserPerpus, idBuku;
+    private Date tanggalPinjam, tanggalKembali;
+    
+    public Peminjaman(){
+        
+    }
+
+    public int getIdPeminjaman() {
+        return idPeminjaman;
+    }
+
+    public int getIdUserPerpus() {
+        return idUserPerpus;
+    }
+
+    public int getIdBuku() {
+        return idBuku;
+    }
+
+    public Date getTanggalPinjam() {
+        return tanggalPinjam;
+    }
+
+    public Date getTanggalKembali() {
+        return tanggalKembali;
+    }
+
+    public void setIdPeminjaman(int idPeminjaman) {
+        this.idPeminjaman = idPeminjaman;
+    }
+
+    public void setIdUserPerpus(int idUserPerpus) {
+        this.idUserPerpus = idUserPerpus;
+    }
+
+    public void setIdBuku(int idBuku) {
+        this.idBuku = idBuku;
+    }
+
+    public void setTanggalPinjam(Date tanggalPinjam) {
+        this.tanggalPinjam = tanggalPinjam;
+    }
+
+    public void setTanggalKembali(Date tanggalKembali) {
+        this.tanggalKembali = tanggalKembali;
+    }
     
 }
