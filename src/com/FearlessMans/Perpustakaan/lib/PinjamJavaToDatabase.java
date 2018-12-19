f0b95c78b75f9fa619109dfe12edc1a0d8a70f6d
KhosyAkmal <akmalkhosy@gmail.com>
2018-12-19 23:12:41 +0700

    buat class pinjam to database

---
+++ src/com/FearlessMans/Perpustakaan/lib/PinjamJavaToDatabase.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.FearlessMans.Perpustakaan.lib;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author khosy
+ */
+public class PinjamJavaToDatabase {
+     
+    public Buku getBuku(int id) {
+        Connection koneksi = Koneksi.buka_koneksi();
+        try {
+            Statement stmt = koneksi.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM peminjaman WHERE id_user_perpustakaan ="+id);
+            
+            if (rs.next()){
+//                Buku buku = new Buku();
+                
+                return extractUserFromResultSet(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       return null; 
+    }
+    
+    
+    private buku extractUserFromResultSet(ResultSet rs) throws SQLException{
+        Buku buku = new Buku();
+        
+        buku.setId(rs.getInt("id_buku"));
+        buku.setJudul(rs.getString("judul_buku"));
+        buku.setPengarang(rs.getString("pengarang_buku"));
+        buku.setIdKategory(rs.getInt("id_category_buku"));
+        buku.setJumlahBuku(rs.getInt("jumlah_buku"));
+        return buku;
+    }
+    
+    public ArrayList getAllBuku() {
+        Connection koneksi = Koneksi.buka_koneksi();
+        try {
+            Statement stmt = koneksi.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM peminjaman");
+            
+            ArrayList bukuBuku = new ArrayList();
+            
+            while (rs.next()){
+                Buku buku = extractUserFromResultSet(rs);
+                bukuBuku.add(buku);
+            }
+            return bukuBuku;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public boolean insertBuku(Buku buku) {
+        Connection koneksi = Koneksi.buka_koneksi();
+        try {
+            PreparedStatement ps = koneksi.prepareStatement("INSERT INTO buku(judul_buku, pengarang_buku, id_category_buku, jumlah_buku) VALUES (?,?,?,?)");
+            ps.setString(1, buku.getJudul());
+            ps.setString(2, buku.getPengarang());
+            ps.setInt(3, buku.getIdKategory());
+            ps.setInt(4, buku.getJumlahBuku());
+            
+            int i = ps.executeUpdate();
+            
+            if (i == 1){
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+//    public boolean updateBuku(Buku buku) {
+//        Connection koneksi = Koneksi.buka_koneksi();
+//        
+//        try {
+//            PreparedStatement ps = koneksi.prepareStatement("UPDATE buku SET judul_buku=?, pengarang_buku=?, id_category_buku=?, jumlah_buku=? WHERE id=?");
+//            ps.setString(1, buku.getJudul());
+//            ps.setString(2, buku.getPengarang());
+//            ps.setInt(3, buku.getIdKategory());
+//            ps.setInt   (4, buku.getJumlahBuku());
+//            ps.setInt   (5, buku.getId());
+//            
+//            int i = ps.executeUpdate();
+//            
+//            if (i == 1){
+//                return true;
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+
+
+//    public boolean deleteBuku(int id) {
+//        Connection koneksi = Koneksi.buka_koneksi();
+//        
+//        try {
+//            Statement stmt = koneksi.createStatement();
+//            int i = stmt.executeUpdate("DELETE FROM buku WHERE id_buku="+id);
+//            if(i == 1){
+//                return true;
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+}
